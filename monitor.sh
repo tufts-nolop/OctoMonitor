20c2a46ac05e7b7dbd99d2da98234bc656a48d38
Deliagwath <ppl3000@gmail.com>
2021-12-01 12:01:11 -0500

    Changed bash script to adhere to same credentials format

--- monitor.sh
+++ monitor.sh
@@ -1,36 +1,17 @@
 #!/bin/bash
-user=$(cat credentials | jq -r '.user');
-pass=$(cat credentials | jq -r '.pass');
-urls=$(cat credentials | jq -r '.urls[]');
-
-for url in ${urls}; do
+input="credentials";
+while read -r line; do
+  data=($line);
   if [ $# -gt 0 ]; then
-    if [[ ${url} != *${1}* ]]; then
+    if [[ ${data} != *${1}* ]]; then
       continue
     fi
   fi
-  cookie=${url#"https://"}
-  cookie=${cookie%"/"}
-  if [ -f "$cookie.cookie" ]; then
-    continue;
-  fi
-  curl -H "Content-Type: application/json" "$url/api/login" -d "{\"user\":\"$user\", \"pass\":\"$pass\", \"remember\":true}" --cookie-jar "$cookie.cookie";
-done;
-
-for url in ${urls}; do
-  if [ $# -gt 0 ]; then
-    if [[ ${url} != *${1}* ]]; then
-      continue
-    fi
-  fi
-  cookie=${url#"https://"}
-  cookie=${cookie%"/"}
-  ret=$(curl -s "$url/api/job" --cookie "$cookie.cookie");
-  echo $url;
+  echo ${data[0]};
+  ret=$(curl -s -H "X-Api-Key:${data[1]}" "https://${data[0]}/api/job");
   echo $ret | jq -r '.state';
   echo "$(echo $ret | jq -r '.progress.completion')%";
   left=$(echo $ret | jq -r '.progress.printTimeLeft');
   echo "$(echo "scale=2 ; $left / 60" | bc) minutes left.";
   echo;
-done;
-
+done < $input;
