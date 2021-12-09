e1cee9b21aa0bc01e23ec4b491d8b53e9c9a557e
Deliagwath <ppl3000@gmail.com>
2021-12-09 10:18:22 -0500

    [Flags] added optargs to handle negate flags, ignoring urls with a certain pattern

--- monitor.sh
+++ monitor.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 input="credentials";
+
+negates=();
+while getopts ":n:" opt; do
+  case $opt in
+    n) negates+=("$OPTARG");;
+  esac
+done
+shift $((OPTIND -1));
+
+skip=0;
 while read -r line; do
   data=($line);
   if [ -z $data ]; then
     continue;
   fi
-  if [ $# -gt 0 ]; then
-    if [[ ${data} != *${1}* ]]; then
-      continue
+  for neg in "${negates[@]}"; do
+    if [[ ${data[0]} =~ .*$neg.* ]]; then
+      skip=1;
+      break;
     fi
+  done
+  if [ $skip -eq 1 ]; then
+    skip=0;
+    continue;
   fi
   echo ${data[0]};
   ret=$(curl -s -H "X-Api-Key:${data[1]}" "https://${data[0]}/api/job");
@@ -18,3 +33,4 @@ while read -r line; do
   echo "$(echo "scale=2 ; $left / 60" | bc) minutes left.";
   echo;
 done < $input;
+
